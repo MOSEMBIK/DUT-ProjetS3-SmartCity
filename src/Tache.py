288c096203734f1b5c3697a4676089565e79d716
Augustin Hazard <hazard.augustin@gmail.com>
2021-10-19 12:04:43 +0200

    Create Tache.py

---
+++ src/Tache.py
@@ -0,0 +1,14 @@
+class Tache:
+    "Définition d'une tâche du projet"
+
+    def __init__(self, idTache, temps, reward):
+        self.id=idTache
+        self.duree=temps
+        self.recompense=reward
+
+    def demandeTache():
+            
+
+
+    def setRecompense(self, nouvelle):
+        self.recompense=nouvelle
