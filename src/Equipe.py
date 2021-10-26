a5ab0b5add9706c95c312a154795cb43c0c712fe
Augustin Hazard <hazard.augustin@gmail.com>
2021-10-26 15:06:12 +0200

    Update Equipe.py

--- src/Equipe.py
+++ src/Equipe.py
@@ -1,1 +1,12 @@
+import Agent
 
+class equipe:
+
+    def __init__(self, id: int, nom: str, agents: list):
+        self.id = id
+        self.nom = nom
+        self.agents = agents
+
+    
+    def getAgents(self) -> list:
+        return self.agents
