954d134ad5b90fa27bb6fb3b8469d67c405be211
MOSEMBIK <71591390+MOSEMBIK@users.noreply.github.com>
2021-11-13 16:20:45 +0100

    Update Agent.py

--- src/Agent.py
+++ src/Agent.py
@@ -42,34 +42,87 @@ class Agent:
 
         self.score = 0
 
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~      METHODES      ~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    def initTrajet(self, environnement: Environnement, positionL: Lieu, position: int, destinationL: Lieu, destination: int) -> None:
-        size = environnement.getDimensions()
-        lieux = environnement.getLieux()
-        
-        # Calcul vecteur Position->Destination
-        def calcVec(p1 ,p2):
-            dist = sqrt( (p1.getCoordonnes[0] - p2.getCoordonnes[0])**2 + (p1.getCoordonnes[1] - p2.getCoordonnes[1])**2 )
-            return dist
+    def move(self) -> None:
+        """
+        Change la position de l'Agent sur la
+        prochaine Case de son trajet.
+        """
+        if self.caseOfTrajet + 1 < len(self.trajet):
+            self.caseOfTrajet += 1
 
-        vec = calcVec(destinationL.getPattern()[destination], positionL.getPattern()[position])
+        elif self.caseOfTrajet + 1 >= len(self.trajet):
+            self.trajet = [self.trajet[self.caseOfTrajet]]
+            self.caseOfTrajet = 0
 
-        if destinationL.getPattern()[destination] and positionL.getPattern()[position] :
-            for i in range(4):
-                # En reflexion
-                i = 0
-
-        else :
-            raise Exception ("Error")
         return None
 
-    def move(self) -> Case:
-        if self.caseOfTrajet + 1 < len(self.trajet):
-            nextCase = self.trajet[self.caseOfTrajet + 1]
-            self.caseOfTrajet += 1
-        elif self.caseOfTrajet + 1 >= len(self.trajet):
-            self.caseOfTrajet = 0
-            self.trajet = [self.caseOfTrajet]
-            nextCase = self.trajet[self.caseOfTrajet]
+    def randomTrajet(self, environnement: Environnement) -> None:
+        """
+        Génère un trajet aléatoire de 0 à 99 déplacements.
+        """
+        roads = environnement.getRoads()
 
-        return nextCase
+        if self.trajet == [] :
+            startId, startCase = random.choice(list(roads.items()))
+            self.trajet.append(startCase)
+        else :
+            onCase = self.trajet[self.caseOfTrajet]
+            self.trajet = [onCase]
+            self.caseOfTrajet = 0
+
+        for i in range(randint(100)) :
+            nextId, nextCase = random.choice(self.trajet[i].nearRoads())
+            self.trajet.append(nextCase)
+        
+        return None
+
+
+    def initTrajet_aStar(self, environnement: Environnement, destinationId : int, destination: Case) -> None:
+        """
+        Génère le trajet le plus court vers
+        une Case donnée en utilisant la methode
+        type de l'algorithme a*.
+        """
+        lastPos = self.trajet[self.caseOfTrajet]
+
+        startXY = self.trajet[self.caseOfTrajet].getCoordonnees()
+        endXY = destination.getCoordonnees()
+        manhattanDist = abs(endXY[0]-startXY[0]) + abs(endXY[1]-startXY[1])
+        
+        done = 0
+        toDo = manhattanDist
+
+        # Génération plus court chemin
+        while toDo != 0 :
+            dist = []
+            lastPos = self.trajet[done]
+            nearRoads = self.trajet[self.caseOfTrajet].nearRoads()
+            nearRoads.remove(lastPos)
+            for nC in nearRoads :
+                nCXY = nC.getCoordonnees()
+                dist.append(abs(endXY[0]-nCXY[0]) + abs(endXY[1]-nCXY[1]))
+        
+            self.trajet.append( nearRoads[dist.index(min(dist))] )
+
+            # Modification de la distance
+            startXY = self.trajet[self.caseOfTrajet].getCoordonnees()
+            toDo = abs(endXY[0]-startXY[0]) + abs(endXY[1]-startXY[1])
+            done += 1
+
+        return None
+
+
+    def initTrajet_dijkstra(self, environnement: Environnement, destinationId : int, destination: Case) -> None:
+        """
+        Génère le trajet le plus court vers
+        une Case donnée.
+        """
+        roads = environnement.getRoads()
+        
+        # Génération plus court chemin
+
+        return None
