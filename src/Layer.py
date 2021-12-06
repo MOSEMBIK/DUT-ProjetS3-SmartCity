ab9de57a069863442d6d8b53680ce18900b113c1
Bambino <melagomen@gmail.com>
2021-12-06 13:56:40 -0500

    Tentative d'ajouter le tableau des scores

--- src/Layer.py
+++ src/Layer.py
@@ -4,3 +4,8 @@ from src.Interface import *
 class Layer:
     def __init__(self, interface):
         self.itf: Interface = interface
+
+        self.itf.addText('AAAAAAAAAAAAAA', 10, 10)
+
+        #self.itf.addTexte('AAAAAAAAAAAAAA', 10, 10)
+
