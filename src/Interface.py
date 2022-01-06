1a68ac90d5c89c53cce8a73ce72f2d6b7cf0c48f
MOSEMBIK <71591390+MOSEMBIK@users.noreply.github.com>
2022-01-06 15:28:02 +0100

    Update Interface.py

--- src/Interface.py
+++ src/Interface.py
@@ -112,7 +112,7 @@ def updateTab(my_game, agent):
     if agent.tacheToDo is None:
         my_game.insert(parent='', index=agent.id, iid=agent.id, text='',
                        values=(agent.id, str(int(agent.charge / agent.autonomie * 100)) + "%",
-                               agent.trajet[agent.caseOfTrajet].getCoords(),
+                               str(int(agent.trajet[agent.caseOfTrajet].getCoords()[0]))+"; "+str(int(agent.trajet[agent.caseOfTrajet].getCoords()[1])),
                                agent.score,
                                "-", "-", agent.wearing, charge))
     else:
