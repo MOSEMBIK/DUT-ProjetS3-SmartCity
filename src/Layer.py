f8b33d51e97e70020bb6e4f4f41703b38b26a517
MOSEMBIK <71591390+MOSEMBIK@users.noreply.github.com>
2022-01-07 00:13:50 +0100

    Update Layer.py

--- src/Layer.py
+++ src/Layer.py
@@ -33,7 +33,7 @@ class Layer:
         self.label2 = createScoreValue(self.frame2, self.score2)
 
         self.pause = addButton(self.frame3, 0.90, 0.25, "II", lambda: self.setupSleep(10))
-        self.play = addButton(self.frame3, 0.90, 0.35, 'P', lambda: self.setupSleep(0.02))
+        self.play = addButton(self.frame3, 0.90, 0.35, '>', lambda: self.setupSleep(0.02))
         self.accelerate = addButton(self.frame3, 0.90, 0.45, 'x1', lambda: self.setupSleepAccelerate())
 
         # Ajout widgets
